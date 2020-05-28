b58c04d40d618fcfe50ae592584edf2204ac05b9
Jon Grythe Stødle <jonstodle@outlook.com>
2020-05-28 23:07:19 +0200

    Add `l` function to fish

---
+++ fish/functions/l.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/bd/wq5v09f17z7bk0bk2rkqpt440000gn/T//fish.xZSfIj/l.fish @ line 2
+function l --description 'List contents fo directory using long format'
+	exa -al $argv
+end
